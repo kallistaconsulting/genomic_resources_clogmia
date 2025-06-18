194b5cf7035d2a61eaef5eda4588958f188eb9e2
kallistaconsulting <118214691+kallistaconsulting@users.noreply.github.com>
2025-06-18 02:01:48 -0400

    Update setup.sh
    made it the correct file lol

--- drupal/setup.sh
+++ drupal/setup.sh
@@ -1,7 +1,30 @@
-docker build -t genome_browser .
-docker run -d \
-  -e HOST_IP=$(curl -s ifconfig.me) \
-  -p 3838:3838 \
-  -p 3000:3000 \
-  -p 4567:4567 \
-  genome_browser
+# Install Drupal
+mkdir /var/www
+mkdir /var/www/html
+cd /var/www/html
+
+curl -Lo drupal.tar.gz https://www.drupal.org/files/projects/drupal-9.5.11.tar.gz
+tar -xzf drupal.tar.gz --strip-components=1
+rm drupal.tar.gz
+
+cd /var/www/html/themes
+wget https://ftp.drupal.org/files/projects/business-9.1.x-dev.tar.gz
+tar xfv business-9.1.x-dev.tar.gz
+rm business-9.1.x-dev.tar.gz
+
+chown -R www-data:www-data /var/www/html
+
+#Configure NGINX for Drupal
+apt install -y nginx
+cp /home/exouser/drupal/nginx-drupal.conf /etc/nginx/sites-available/default
+
+# Setup MariaDB initial database
+apt install -y php-fpm php-mysql php-gd php-xml php-mbstring php-curl
+apt-get update && apt-get install -y mariadb-server
+
+#echo "###################"
+#echo "password is clogmia"
+#echo "###################"
+
+mysql -u root -p < /home/exouser/drupal/init.sql
+mysql -u root -p drupal < /home/exouser/drupal/drupal-db.sql
